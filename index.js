624792dd58755b35e26e31804cf5c94f1b78f715
Sayantan Das <das.sayantan94@gmail.com>
2016-12-06 00:12:07 +0530

    Halfway working version

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-var io = require('socket.io');
-var socket = io.listen(3000, '127.0.0.1');
+var app = require('express')();
+var http = require('http').Server(app);
+
+var io = require('socket.io')(http);
+// var socket = io.listen(3000, '127.0.0.1');
 
 // all the people that have joined the chat
 var people = {};
 
-socket.on('connection', function (client) {
+io.on('connection', function (client) {
     console.log('An user connected');
     client.on('join', function(name) {
         people[client.id] = name;
@@ -12,18 +15,26 @@ socket.on('connection', function (client) {
         // client.emit() will only update the client that you are looking
         // at, whereas socket.sockets.emti() will update all connected clients
         client.emit('update', 'You have successfully connected..');
-        socket.sockets.emit('update', name + " has joined the conversation..");
-        socket.sockets.emit('update-people', people);
+        io.sockets.emit('update', name + " has joined the conversation..");
+        io.sockets.emit('update-people', people);
     });
 
     client.on('send', function(msg){
-        socket.sockets.emit('chat', people[client.id], msg);
+        io.sockets.emit('chat', people[client.id], msg);
     });
 
     client.on('disconnect', function() {
-        socket.sockets.emit('update', people[client.id] + ' has left the conversation..');
+        io.sockets.emit('update', people[client.id] + ' has left the conversation..');
         delete people[client.id];
-        socket.sockets.emit('update-people', people);
+        io.sockets.emit('update-people', people);
     });
 
 });
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, function() {
+    console.log('Listening on port 3000...');
+})
