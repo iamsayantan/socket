111005f469b5821897f9b02e285e56bcf28e80e3
Sayantan Das <sayantan.das@codelogicx.com>
2017-01-03 15:40:55 +0530

    webhook url in index.js

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var app = require('express')();
 var http = require('http').Server(app);
 
+// validation token for bot
+const VALIDATION_TOKEN = 'myBot_validation_token'
+
 var io = require('socket.io')(http);
 // var socket = io.listen(3000, '127.0.0.1');
 
 // all the people that have joined the chat
 var people = {};
 
-io.on('connection', function (client) {
+io.on('connection', function(client) {
     console.log('An user connected');
     client.on('join', function(name) {
         people[client.id] = name;
@@ -18,7 +21,7 @@ io.on('connection', function (client) {
         io.sockets.emit('update-people', people);
     });
 
-    client.on('send', function(msg){
+    client.on('send', function(msg) {
         console.log(people[client.id] + ' Says: ' + msg);
         io.sockets.emit('chat', people[client.id], msg);
     });
@@ -31,8 +34,24 @@ io.on('connection', function (client) {
 
 });
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+/*
+ * Use your own validation token. Check that the token used in the Webhook 
+ * setup is the same token used here.
+ *
+ */
+app.get('/webhook', function(req, res) {
+    if (req.query['hub.mode'] === 'subscribe' &&
+        req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+        console.log("Validating webhook");
+        res.status(200).send(req.query['hub.challenge']);
+    } else {
+        console.error("Failed validation. Make sure the validation tokens match.");
+        res.sendStatus(403);
+    }
 });
 
 app.set('port', process.env.PORT || 3000);
