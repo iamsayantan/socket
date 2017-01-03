f931b18f4a6d32f3038d7e7d324236ca0a0bfdf4
Sayantan Das <sayantan.das@codelogicx.com>
2017-01-03 18:11:39 +0530

    Add bodyParser

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+/** bodyParser.urlencoded(options)
+ * Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
+ * and exposes the resulting object (containing the keys and values) on req.body
+ */
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+/**bodyParser.json(options)
+ * Parses the text as JSON and exposes the resulting object on req.body.
+ */
+app.use(bodyParser.json());
+
 var app = require('express')();
 var http = require('http').Server(app);
 
