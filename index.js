83484aa72d3121871dafd829bed048a39c933b3c
Sayantan Das <sayantan.das@codelogicx.com>
2017-01-04 16:24:59 +0530

    json parsing

--- index.js
+++ index.js
@@ -202,6 +202,7 @@ function receivedPostback(event) {
     // request for user data and use the data
     getUserDetails(senderID, function(data) {
         console.log(data);
+        data = JSON.parse(data);
         var firstName = data.first_name;
 
         var returnMessage = "Hello " + data.first_name + ", it's nice to meet you!";
