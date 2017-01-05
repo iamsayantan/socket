a7f750a561145ec161cb1bfc859e5da4503cde0c
Sayantan Das <sayantan.das@codelogicx.com>
2017-01-05 17:02:24 +0530

    sdgf

--- index.js
+++ index.js
@@ -324,7 +324,7 @@ function getMyInfo() {
         method: 'GET',
         headers: { 'user-agent': 'node.js' }
     }, function(error, response, body) {
-        // console.log(body);
+        console.log(body);
         body = JSON.parse(body);
         console.log('====================');
         console.log(body);
