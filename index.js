a874fb60169de9d120593914e80de857ca68c8a0
Sayantan Das <das.sayantan94@gmail.com>
2017-01-06 21:31:04 +0530

    debug context

--- index.js
+++ index.js
@@ -209,6 +209,23 @@ function receivedMessage(event) {
                 messageText, // the user's message
                 sessions[sessionId].context // the user's current session state
             )
+            .then((context) => {
+                console.log('This context is from runActions   :', context);
+                // Our bot did everything it has to do.
+                // Now it's waiting for further messages to proceed.
+                console.log('Waiting for next user messages');
+
+                // Based on the session state, you might want to reset the session.
+                // This depends heavily on the business logic of your bot.
+                // Example:
+                // if (context['done']) {
+                //   delete sessions[sessionId];
+                // }
+
+                // Updating the user's current session state
+                sessions[sessionId].context = context;
+                console.log(context);
+            })
             .catch((err) => {
                 console.error('Oops! Got an error from Wit: ', err.stack || err);
             });
