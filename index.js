aab0ae37ed534ff7ae7eb31e1a87ae301ddd99fa
Sayantan Das <sayantan.das@codelogicx.com>
2017-01-09 16:03:26 +0530

    test

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ const actions = {
     sentimentAnalyze({context, entities}) {
         console.log('Contex ==> ', context);
         console.log('Entities ==> ', entities);
-        console.log(entities.sentiment.value);
+        console.log(entities.sentiment);
     }
 };
 
